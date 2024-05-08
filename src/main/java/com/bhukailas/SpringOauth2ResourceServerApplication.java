2cfc7fd008c68469af35bdf53082ec5c58286ea9
bhukailas <bhukailas448@gmail.com>
2024-05-08 18:27:14 -0500

    initial changes

---
+++ src/main/java/com/bhukailas/SpringOauth2ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.bhukailas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhukailas/SpringOauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhukailas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
